fa3bee1b972f4c065f97850ba92a9a46732d17a3
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-03-11 17:32:20 +0530

    ....

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+<h2>Login Window</h2>
+<form action="validation.php" method="post">
+<table>
+	<tr>
+	<td>User_id</td>
+	<td><input type="text" name="User_id"/></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td><input type="Password" name="Password"/></td>
+	</tr>
+	<tr>
+	<td></td>
+	<td><input type="submit" value="Login"/>  <a href="register.php">register</a></td>
+	</tr>
+</table>
+</form>
+
+</body>
+</html>
