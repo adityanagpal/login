f8b81de4afb3f01c1234ed8fef7c208a7043b471
adityanagpal <32595878+adityanagpal@users.noreply.github.com>
2018-03-14 13:46:13 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+<h2>Registeration Window</h2>
+<form action="registeration.php" method="post">
+<table>
+	<tr>
+	<td>User_id</td>
+	<td><input type="text" name="User_id"/></td>
+	</tr>
+	<tr>
+	<td>Password</td>
+	<td><input type="Password" name="Password"/></td>
+	</tr>
+	<tr>
+	<td></td>
+	<td><input type="submit" value="Register"/> </td>
+	</tr>
+</table>
+</form>
+
+</body>
+</html>
